74faab940bc98bb16cbb3f305aa228c95102a6ee
luskalbert <ahlusk@uncg.edu>
2023-10-29 02:14:05 -0400

    created new farmer data

--- crop_trader.py
+++ crop_trader.py
@@ -8,14 +8,21 @@ import getters
 # except player_exceptions.PlayerException as e:
 #     print(e)
 
-print(getters.get_plot_size('86a40223-53e6-44d4-969f-f102a89b81d0'))
-print(getters.get_money('86a40223-53e6-44d4-969f-f102a89b81d0'))
+# print(getters.get_plot_size('86a40223-53e6-44d4-969f-f102a89b81d0'))
+# print(getters.get_money('86a40223-53e6-44d4-969f-f102a89b81d0'))
 # print(getters.get_plots('86a40223-53e6-44d4-969f-f102a89b81d0'))
-print(getters.get_seeds('86a40223-53e6-44d4-969f-f102a89b81d0'))
-print(getters.get_products('86a40223-53e6-44d4-969f-f102a89b81d0'))
-print(getters.get_product_value('86a40223-53e6-44d4-969f-f102a89b81d0'))
-# print(getters.get_net_value('86a40223-53e6-44d4-969f-f102a89b81d0'))
-print(getters.get_net_worth('86a40223-53e6-44d4-969f-f102a89b81d0'))
+# print(getters.get_seeds('86a40223-53e6-44d4-969f-f102a89b81d0'))
+# print(getters.get_products('86a40223-53e6-44d4-969f-f102a89b81d0'))
+# print(getters.get_product_value('86a40223-53e6-44d4-969f-f102a89b81d0'))
+# print(getters.get_net_worth('86a40223-53e6-44d4-969f-f102a89b81d0'))
+
+farmer_actions.create_farmer("Joe")
+farmer_actions.create_farmer("Bob")
+farmer_actions.create_farmer("Lee")
+farmer_actions.create_farmer("Moe")
+farmer_actions.create_farmer("Cox")
+farmer_actions.create_farmer("Ali")
+farmer_actions.create_farmer("Poe")
 
 if __name__ == '__main__':
     app.run()
