5c0f2f4dd42b38918eb597485089fc28c9865c50
sudo-sandwich <luskconrad@gmail.com>
2023-10-29 00:56:59 -0700

    trying to fix cant find exception bug

--- getters.py
+++ getters.py
@@ -1,7 +1,12 @@
 import sb_client
 
+class UUIDNotFoundException(Exception):
+    bleh = 0
+
 def get_plot_size(player_uuid: str) -> int:
     response = sb_client.supabase.table('player_data').select('plot_size').eq('id', player_uuid).execute()
+    if not response.data:
+        raise UUIDNotFoundException(f'UUID {player_uuid} not found in database.')
     return response.data[0]['plot_size']
 
 # def get_plots(player_uuid: str):
@@ -13,23 +18,31 @@ def get_plot_size(player_uuid: str) -> int:
 #     
 #     return my_list
 
-def get_money(player_uuid: str):
+def get_money(player_uuid: str) -> int:
     response = sb_client.supabase.table('player_data').select('money').eq('id', player_uuid).execute()
+    if not response.data:
+        raise UUIDNotFoundException(f'UUID {player_uuid} not found in database.')
     return response.data[0]['money']
 
-def get_seeds(player_uuid: str):
+def get_seeds(player_uuid: str) -> list:
     response = sb_client.supabase.table('player_data').select('seeds').eq('id', player_uuid).execute()
+    if not response.data:
+        raise UUIDNotFoundException(f'UUID {player_uuid} not found in database.')
     return response.data[0]['seeds']
 
-def get_products(player_uuid: str):
+def get_products(player_uuid: str) -> list:
     response = sb_client.supabase.table('player_data').select('products').eq('id', player_uuid).execute()
+    if not response.data:
+        raise UUIDNotFoundException(f'UUID {player_uuid} not found in database.')
     return response.data[0]['products']
 
-def get_product_value(player_uuid: str):
+def get_product_value(player_uuid: str) -> list:
     response = sb_client.supabase.table('player_data').select('product_value').eq('id', player_uuid).execute()
+    if not response.data:
+        raise UUIDNotFoundException(f'UUID {player_uuid} not found in database.')
     return response.data[0]['product_value']
 
-def get_net_worth(player_uuid: str):
+def get_net_worth(player_uuid: str) -> int:
     sum = 0
     my_products = get_products(player_uuid)
     for i in range(len(my_products)):
